09b901d166bca94e50e7778a4237d14d105ffddc
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2022-03-13 14:37:42 +0300

    clear unused api

--- apps/chat/src/app/app.tsx
+++ apps/chat/src/app/app.tsx
@@ -1,19 +1,10 @@
-import { useEffect, useState } from 'react';
-import { Message } from '@blockchain-chat/api-interfaces';
 import { Web3ReactProvider } from '@web3-react/core';
 import Account from './login-page';
 import getLibrary from '../getLibrary';
 import useEagerConnect from '../hooks/useEagerConnect';
 
 export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
   const triedToEagerConnect = useEagerConnect();
-
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
 
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
